8a40e96830a89a822d115204625fb6b52953a578
jeffinhostyle <jeffinho.oliveira94@gmail.com>
2025-07-28 19:49:47 -0300

    Delete main.py

--- main.py
+++
@@ -1,9 +0,0 @@
-import json
-
-def carregar_dados():
-    with open('access.json', 'r') as f:
-        dados = json.load(f)
-        print("Bot iniciado com token:", dados['telegram_token'])
-
-if __name__ == '__main__':
-    carregar_dados()
